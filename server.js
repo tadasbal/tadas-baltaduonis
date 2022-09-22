4cf41a1c516455e20698b8ee42e2b6d6cc34e5e1
Tadas Baltaduonis <tadasbaltaslt@gmail.com>
2022-09-22 11:17:21 +0300

    initial app commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+//serve static file (index.html, images, css)
+app.use(express.static(__dirname + '/views'));
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
